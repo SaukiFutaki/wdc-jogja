1ea3b5f2baa5ee6d712670da9febc386e48e5c67
SaukiFutaki <utaki.sauki19@gmail.com>
2025-02-25 22:01:13 +0700

    feat(navbar): add responsive navbar component

---
+++ components/navbar/nav-sheet.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Logo from "../logo";
+import { NavMenu } from "./nav-menu";
+import Image from "next/image";
+
+export const NavigationSheet = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon" className="rounded-full">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+       <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={200}
+            height={40}
+          
+       />
+        <NavMenu orientation="vertical" className="mt-12" />
+      </SheetContent>
+    </Sheet>
+  );
+};
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import Logo from "../logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./nav-sheet";
+import Image from "next/image";
+export default function Navbar() {
+  return (
+    <div className="min-h-screen bg-muted">
+    <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-screen-xl mx-auto rounded-full">
+      <div className="h-full flex items-center justify-between mx-auto px-4">
+            <Image
+                   src="/logo.svg"
+                   alt="Logo"
+                   width={200}
+                   height={40}
+               
+              />
+        {/* Desktop Menu */}
+        <NavMenu className="hidden md:block" />
+        <div className="flex items-center gap-3">
+          <Button
+            variant="outline"
+            className="hidden sm:inline-flex rounded-full"
+          >
+            Sign In
+          </Button>
+          <Button className="rounded-full hidden sm:inline-flex">Get Started</Button>
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <NavigationSheet />
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  )
+}
