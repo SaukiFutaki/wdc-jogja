7c90f3bfe834482465e7cf497ced963e3df60fd6
SaukiFutaki <utaki.sauki19@gmail.com>
2025-02-25 23:04:03 +0700

    feat(env) : base url env

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000" // the base url of your auth server
+    baseURL: process.env.BETTER_AUTH_URL // the base url of your auth server
 })
 
 export const signIn = async () => {
